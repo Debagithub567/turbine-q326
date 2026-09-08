f65a1c2668a8fd83add998795caddf9fc7d9da5f
Debajit Roul <debajitroul181@gmail.com>
2026-09-08 22:45:54 +0530

    some changes in withdraw.rs

--- programs/vault/src/instructions/withdraw.rs
+++ programs/vault/src/instructions/withdraw.rs
@@ -1,11 +1,19 @@
 use anchor_lang::prelude::*;
 use crate::VaultState;
 
-
 #[derive(Accounts)]
-pub struct Withdraw<'info>{
+pub struct Withdraw<'info> {
     pub user: Signer<'info>,
-    pub vautl_state: Account<'info, VaultState>,
+    pub vault_state: Account<'info, VaultState>,
+
+    #[account(
+        seeds = [
+            b"vault_asset",
+            vault_state.owner.as_ref()
+        ],
+        bump
+    )]
     pub vault_asset: SystemAccount<'info>,
-    pub system_program: Program<'info , System>,
+
+    pub system_program: Program<'info, System>,
 }
